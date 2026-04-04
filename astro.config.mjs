bc9785aea65156150a5465178c638d1da6a29b7e
abhisheksagar310 <abhisheksagar310@users.noreply.github.com>
2026-04-04 22:09:51 +0530

    Fix domain from mavek.io to mavek.ai

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mavek.io',
+  site: 'https://mavek.ai',
   integrations: [sitemap()],
 });
